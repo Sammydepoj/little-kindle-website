6a2a8b01e1b821ff8ef36e35a921ef3fbf0f8615
Samuel Adepoju <samueladepoju86@yahoo.com>
2024-08-22 08:57:36 +0100

    chore: refactor

---
+++ src/components/CardWithImage/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import CustomButton from "../Button";
+import ArrowIcon from "@/assets/icons/ArrowIcon";
+import ShineMark from "@/assets/icons/ShineMark";
+import Image from "next/image";
+
+type Props = {
+  image: any;
+  title: string;
+  description: string;
+  learnMoreAction?: () => void;
+  showLearnMore: boolean;
+  imageAlt: string;
+  reverse?: boolean;
+};
+const CardWithImage = ({
+  description,
+  image,
+  showLearnMore,
+  learnMoreAction,
+  title,
+  imageAlt,
+  reverse,
+}: Props) => {
+  return (
+    <div
+      className={`flex gap-6 my-8 flex-wrap md:flex-nowrap w-full ${
+        reverse && "flex-row-reverse"
+      }`}
+    >
+      <Image src={image} alt={imageAlt} />
+      <div className="bg-[#F1F1F3] p-2 md:p-6 rounded-2xl grid">
+        <div className="flex items-center justify-between flex-wrap gap-4 my-4">
+          <span className="flex items-center gap-3">
+            <ShineMark />
+            <h4 className="text-[#1A1A1A] font-semibold text-2xl md:text-3xl">
+              {title}
+            </h4>
+          </span>
+
+          {showLearnMore && (
+            <CustomButton
+              text="Learn More"
+              icon={<ArrowIcon stroke="#3A5774" />}
+              className="!bg-[#FFFFFF] !text-[#3A5774]"
+              onClick={learnMoreAction}
+            />
+          )}
+        </div>
+        <p className="bg-[#FFFFFF] mx-auto p-2 md:p-6 rounded-2xl text-base font-normal">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CardWithImage;
---
+++ src/components/HeroCard/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import CustomButton from "../Button";
+import ArrowIcon from "@/assets/icons/ArrowIcon";
+import Image from "next/image";
+
+type Props = {
+  image: any;
+  cardTitle: string;
+  cardDescription: string;
+  showButtons: boolean;
+  learnMoreAction?: () => void;
+  donateNowAction?: () => void;
+  imageAlt?: string;
+};
+
+const HeroCard = ({
+  cardDescription,
+  cardTitle,
+  image,
+  showButtons,
+  donateNowAction,
+  learnMoreAction,
+  imageAlt,
+}: Props) => {
+  return (
+    <div>
+      <Image alt={imageAlt ?? ""} src={image} className="-z-30" />
+      <div className="bg-[#F7F7F8] p-2 md:py-8 md:px-11 rounded-2xl max-w-[980px] mx-auto mt-[-2%]  sm:mt-[-5%] z-[90] relative">
+        <h1 className="text-[#1A1A1A] font-semibold text-2xl sm:text-3xl md:text-5xl text-center ">
+          {cardTitle}
+        </h1>
+        <p className="text-base text-[#4C4C4D] font-normal text-center my-3 md:my-8">
+          {cardDescription}
+        </p>
+        {showButtons && (
+          <div className="mx-auto flex items-center flex-wrap gap-8 md:w-max">
+            <CustomButton
+              text="Learn More"
+              icon={<ArrowIcon stroke="#3A5774" />}
+              className="!text-[#3A5774] !bg-[#FFFFFF]"
+              onClick={learnMoreAction}
+            />
+            <CustomButton
+              text="Donate Now"
+              icon={
+                <span className="bg-[#3A5774] p-2 rounded-full">
+                  <ArrowIcon stroke="#FFFFFF" />
+                </span>
+              }
+              className="!p-2"
+              onClick={donateNowAction}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HeroCard;
